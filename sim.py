303fbfb577cc61ffa100b50e259a38f6fd40d730
Mateusz Krawiec <mateusz.krawiec6@gmail.com>
2019-08-01 18:25:33 +0200

    Implement an actual Element type able to store symbolic representation of a complex element

--- sim.py
+++ sim.py
@@ -18,7 +18,29 @@ SUPPORTED_COMPONENTS = {
 }
 
 
-class Edge(collections.namedtuple('Edge', ['u', 'v', 'symbol'])):
+class Element:
+    def __init__(self, symbol, symbolic_expression):
+        self._symbol = symbol
+        self._value = symbolic_expression
+
+    @property
+    def symbol(self):
+        return self._symbol
+
+    @property
+    def type(self):
+        return str(self.symbol)[0]
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def terms(self):
+        return self._value.as_terms()[-1]
+
+
+class Edge(collections.namedtuple('Edge', ['u', 'v', 'element'])):
     pass
 
 
@@ -34,21 +56,23 @@ def parse_edge(n1: str, n2: str, c: str):
     if c[0] not in SUPPORTED_COMPONENTS:
         raise ValueError("Unsupported component: %s" % c)
 
-    return Edge(u=n1, v=n2, symbol=sympy.symbols(c))
+    e = sympy.sympify(c)
+    return Edge(n1, n2, Element(e, e))
 
 
-class EdgeSet(set):
+class Network:
     def __init__(self):
         super().__init__()
-        self._ground_node = None
+        self._edges = set()
         self._nodes = set()
+        self._ground_node = None
         self._incident_edges_of_node = collections.defaultdict(list)
 
-    def add(self, elem):
+    def add_edge(self, elem: Edge):
         if not isinstance(elem, Edge):
             raise ValueError(
                 "Only items of class Edge can be added to the EdgeSet")
-        elif elem in self:
+        elif elem in self._edges:
             return
 
         u, v, _ = elem
@@ -61,16 +85,16 @@ class EdgeSet(set):
         self._nodes.add(v)
         self._incident_edges_of_node[u] += [elem]
         self._incident_edges_of_node[v] += [elem]
-        super().add(elem)
-
-    def remove(self, elem):
-        raise NotImplementedError
+        self._edges.add(elem)
 
     def unordered_nodes(self):
         return self._nodes
 
     def nodes(self):
         return sorted(self._nodes, key=str)
+
+    def edges(self):
+        return self._edges
 
     def nonground_nodes(self):
         return filter(lambda u: u != self.ground_node(), self.nodes())
@@ -84,43 +108,37 @@ class EdgeSet(set):
         return self._incident_edges_of_node[node]
 
 
-def parse_network() -> EdgeSet:
-    edges = EdgeSet()
+def parse_network() -> Network:
+    net = Network()
     for line in sys.stdin:
         items = line.strip().split()
         if len(items) == 3:
-            edges.add(parse_edge(*items))
-        # elif len(items) == 2:
-        #    parse_inout(*items)
+            net.add_edge(parse_edge(*items))
 
-    return edges
+    return net
 
 
-def element_type(symbol):
-    return str(symbol)[0]
-
-
-def solve_system(edges: EdgeSet):
-    ground_node = edges.ground_node()
+def solve_system(net: Network):
+    ground_node = net.ground_node()
 
     # Elem is either a node or a supernode (e.g. voltage source).
     elem_index = dict()
-    for index, node in enumerate(edges.nonground_nodes()):
+    for index, node in enumerate(net.nonground_nodes()):
         elem_index[node] = index
 
     # Every voltage source introduces one unknown current through
     # it, but also one additional equation, thus keeping the system
     # solvable.
-    for edge in (e for e in edges if element_type(e.symbol) == 'V'):
-        elem_index[edge.symbol] = len(elem_index)
+    for edge in (e for e in net.edges() if e.element.type == 'V'):
+        elem_index[edge.element.symbol] = len(elem_index)
 
     n = len(elem_index)
     G = sympy.zeros(n, n)
     b = sympy.zeros(n, 1)
 
-    for row, node in enumerate(edges.nonground_nodes()):
-        for edge in edges.incident_edges(node):
-            u, v, symbol = edge
+    for row, node in enumerate(net.nonground_nodes()):
+        for edge in net.incident_edges(node):
+            u, v, element = edge
             # From now on, assume `node` == u, and that the current through
             # passive components always flows into that node.
             if node == v:
@@ -129,21 +147,20 @@ def solve_system(edges: EdgeSet):
             else:
                 polarity = -1
 
-            elem_type = element_type(symbol)
-            if elem_type == 'R':
+            if element.type == 'R':
                 # Current flows into u, we thus have a factor:
                 # (Vv - Vu) * gx
-                g = 1 / symbol
+                g = 1 / element.value
                 assert u != ground_node
                 G[row, elem_index[u]] -= g
                 if v is not ground_node:
                     G[row, elem_index[v]] += g
-            elif elem_type == 'I':
-                b[elem_index[node]] = -symbol
-            elif elem_type == 'V':
-                G[row, elem_index[symbol]] = polarity
-                G[elem_index[symbol], elem_index[node]] = polarity
-                b[elem_index[symbol]] = symbol
+            elif element.type == 'I':
+                b[elem_index[node]] = -element.value
+            elif element.type == 'V':
+                G[row, elem_index[element.symbol]] = polarity
+                G[elem_index[element.symbol], elem_index[node]] = polarity
+                b[elem_index[element.symbol]] = element.symbol
             else:
                 raise NotImplementedError
 
